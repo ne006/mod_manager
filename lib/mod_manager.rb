bf50867c300041b6eff0fb6e115ab43b0ecc6cd0
ne006 <IATikhomirov@gmail.com>
2020-08-02 18:48:18 +0300

    Confugartion and executable

--- lib/mod_manager.rb
+++ lib/mod_manager.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
 require 'mod_manager/version'
+
+require 'mod_manager/cli'
+
 require 'mod_manager/instance'
 
+require 'config'
+
+config_file_path = if File.exist?('mod_manager.yml')
+                     'mod_manager.yml'
+                   else
+                     Pathname.new(__FILE__).ascend.take(3).last.join('mod_manager.yml')
+                   end
+
+Config.load_and_set_settings(config_file_path)
+
+# Top-level module and entrypoint
 module ModManager
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def cli
+      ModManager::CLI.new.start(ARGV)
+    end
+  end
 end
--- mod_manager.gemspec
+++ mod_manager.gemspec
@@ -34,5 +34,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec', '~> 3.8'
   spec.add_development_dependency 'rubocop'
 
+  spec.add_dependency 'config'
   spec.add_dependency 'rubyzip'
 end
